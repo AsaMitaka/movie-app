582e3374f51496044210dc9d958b16aacca6fb3c
AsaMitaka <cepelits@gmail.com>
2023-08-27 14:44:21 +0300

    add type to elements

--- src/components/Element.tsx
+++ src/components/Element.tsx
@@ -1,18 +1,21 @@
 import { Link } from 'react-router-dom';
+import { ElementProps } from '../types/trending';
 
-const Element = ({ item }) => {
+const Element: React.FC<ElementProps> = ({ item }) => {
+  console.log(item.name, item.media_type);
+
   return (
     <div className="element">
       <img
         src={`https://image.tmdb.org/t/p/original/${item?.poster_path}`}
-        alt={item?.title}
+        alt={item.media_type === 'tv' ? item?.name : item?.original_title}
         className="element__img"
       />
       <Link
         to={item.media_type === 'tv' ? `/show/${item.id}` : `/movie/${item.id}`}
         className="element__block">
         <div className="element__block--title">
-          {item.media_type === 'tv' ? item.name : item.original_title}
+          {item.media_type === 'tv' ? item?.name : item.original_title}
         </div>
         <div className="element__block--subtitle">Drama</div>
       </Link>
